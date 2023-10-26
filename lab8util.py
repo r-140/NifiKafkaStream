fc4c99a3927b80c7bc5f40a749150b9f11cf44e2
Ilya Ushakov <ilya.ushakov@globallogic.com>
2023-10-26 18:30:41 +0200

    code refactoring

--- lab8util.py
+++ lab8util.py
@@ -3,6 +3,7 @@ import pyspark.sql.functions as f
 
 def get_total_price_and_sales(df):
     return df.withColumn("sales", f.col("price") * f.col("amount")) \
+        .withWatermark("event_time", "1 minute") \
         .groupBy(f.window("event_time", "1 minute")) \
         .agg({'price': 'sum', 'sales': 'sum'})
 
